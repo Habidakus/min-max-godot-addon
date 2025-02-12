d9f19b70f3308aa465b85bee50755d214f84c9b6
Jason Abbott <woebane@gmail.com>
2025-02-12 15:06:02 -0800

    Added documentation for the state machine addon

--- addons/statemachine/StateMachine.gd
+++ addons/statemachine/StateMachine.gd
@@ -1,41 +1,50 @@
-extends Node
+class_name StateMachine extends Node
+## The manager class for a control node state machine.
+##
+## All states (implementing [StateMachineState]) for this state machine should live as immediate
+## children of the [StateMachine] node in the tree.
+##
+## An example of this code being used can be found at https://github.com/Habidakus/min-max-godot-addon
+## where this addon is also located.
 
-class_name StateMachine
-
+## Indicates which [StateMachineState] instance should be the start state when the state machine starts up, 
 @export var initial_state : StateMachineState = null
 
+## A list of all registered states
 var all_states : Array = []
+## Which state is currently running
 var current_state : StateMachineState = null
 
 func _ready() -> void:
 	for child in get_children():
 		if child is StateMachineState:
-			register_state(child)
-	switch_state_internal(initial_state)
+			_register_state(child)
+	_switch_state_internal(initial_state)
 
-func register_state(state: StateMachineState) -> void:
+func _register_state(state: StateMachineState) -> void:
 	all_states.append(state)
 	state.init_state(self)
 
-func begin_state(state: StateMachineState) -> void:
+func _begin_state(state: StateMachineState) -> void:
 	current_state = state
 	if state != null:
 		state.enter_state()
 
-func switch_state_internal(state: StateMachineState) -> void:
+func _switch_state_internal(state: StateMachineState) -> void:
 	if current_state == state:
 		print("Already in state " + state.name + ", aborting state switch")
 		return
 	if current_state != null:
-		var callback : Callable = Callable(self, "begin_state")
-		current_state.set_completion_call(callback.bind(state))
+		var callback : Callable = Callable(self, "_begin_state")
+		current_state._set_completion_call(callback.bind(state))
 		current_state.exit_state(state)
 	else:
-		begin_state(state)
+		_begin_state(state)
 
+## Called when the state machine should transition to another state
 func switch_state(next_state_name: String) -> void:
 	for state : StateMachineState in all_states:
 		if state.name == next_state_name:
-			switch_state_internal(state)
+			_switch_state_internal(state)
 			return
 	assert(false, "{0} is not a valid state".format([next_state_name]))
--- addons/statemachine/StateMachineState_AnyKeyToContinue.gd
+++ addons/statemachine/StateMachineState_AnyKeyToContinue.gd
@@ -1,64 +1,70 @@
 class_name StateMachineState_PressAnyKey extends StateMachineState
+## Simple control state that will stay up until the user hits a key or clicks a mouse
 
+## This is the next control state that we should transition to after the user hits a key
 @export var next_state : StateMachineState
+## If positive then the page will automatically transition in this many seconds, regardless of key press
 @export var time_out_in_seconds : float = -1
 
+## If we want to fade this page in, set this to true
 @export var fade_in : bool = false
+## If we want to fade this page out on transition to the next page, set this to true
 @export var fade_out : bool = false
+## Dictates how many seconds the page will spend both fading in and fading out
 @export var fade_time : float = 1.5
 
-var countdown : float = 0
-var leave_tween : Tween = null
+var _countdown : float = 0
+var _leave_tween : Tween = null
 
 func _process(delta: float) -> void:
 	if time_out_in_seconds > 0:
-		countdown += delta
-		if countdown > time_out_in_seconds:
-			our_state_machine.switch_state_internal(next_state)
+		_countdown += delta
+		if _countdown > time_out_in_seconds:
+			our_state_machine.switch_state(next_state.name)
 
 func _input(event : InputEvent) -> void:
-	handle_event(event)
+	_handle_event(event)
 
 func _unhandled_input(event : InputEvent) -> void:
-	handle_event(event)
+	_handle_event(event)
 
-func handle_event(event : InputEvent) -> void:
+func _handle_event(event : InputEvent) -> void:
 	# We process on "released" instead of pressed because otherwise immediately
 	# switching screens could still have the mouse being pressed on some other
 	# screen's button.
 	if process_mode == ProcessMode.PROCESS_MODE_DISABLED:
 		return
 		
-	if leave_tween == null:
+	if _leave_tween == null:
 		if event.is_released():
 			if event is InputEventKey:
-				our_state_machine.switch_state_internal(next_state)
+				our_state_machine.switch_state(next_state.name)
 			if event is InputEventMouseButton:
-				our_state_machine.switch_state_internal(next_state)
+				our_state_machine.switch_state(next_state.name)
 
 func exit_state(next_state: StateMachineState) -> void:
 	if !fade_out:
 		super.exit_state(next_state)
 		return
 	
-	if leave_tween != null && leave_tween.is_running():
+	if _leave_tween != null && _leave_tween.is_running():
 		return
 
-	leave_tween = get_tree().create_tween()
+	_leave_tween = get_tree().create_tween()
 	self.modulate = Color(Color.WHITE, 1)
 	var destination_color : Color = Color(Color.WHITE, 0)
-	leave_tween.tween_property(self, "modulate", destination_color, fade_time)
-	var when_finished_callback : Callable = Callable(self, "on_leave_tween_finished")
-	leave_tween.tween_callback(when_finished_callback.bind(next_state))
+	_leave_tween.tween_property(self, "modulate", destination_color, fade_time)
+	var when_finished_callback : Callable = Callable(self, "_on_leave_tween_finished")
+	_leave_tween.tween_callback(when_finished_callback.bind(next_state))
 
-func on_leave_tween_finished(next_state: StateMachineState) -> void:
+func _on_leave_tween_finished(next_state: StateMachineState) -> void:
 	super.exit_state(next_state)
-	leave_tween = null
+	_leave_tween = null
 
 func enter_state() -> void:
 	super.enter_state()
-	countdown = 0
-	leave_tween = null
+	_countdown = 0
+	_leave_tween = null
 	if fade_in:
 		self.modulate = Color(Color.WHITE, 0)
 		var tween : Tween = get_tree().create_tween()
